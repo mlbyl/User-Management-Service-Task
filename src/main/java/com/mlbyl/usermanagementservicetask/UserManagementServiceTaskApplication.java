0485f05aab00cbf6db9e827807d772eba1921cb5
Memed555 <mehemmedelibeyli555@gmail.com>
2025-11-16 14:48:08 +0400

    Initial commit

---
+++ src/main/java/com/mlbyl/usermanagementservicetask/UserManagementServiceTaskApplication.java
@@ -0,0 +1,13 @@
+package com.mlbyl.usermanagementservicetask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementServiceTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserManagementServiceTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mlbyl/usermanagementservicetask/UserManagementServiceTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mlbyl.usermanagementservicetask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementServiceTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
